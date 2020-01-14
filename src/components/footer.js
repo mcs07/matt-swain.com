e9615fdbdefc1cb90c1108ddfa9d3165fa892443
Matt Swain <m.swain@me.com>
2020-01-14 22:45:12 +0000

    Better footer wrap on mobile

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,21 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { Container, Flex } from "@theme-ui/components"
+import { Box, Container } from "@theme-ui/components"
 import { NavLink } from "./link"
 
 export const Footer = props => (
   <Container as="footer" py={5} {...props}>
-    <Flex sx={{ flexWrap: "wrap" }}>
-      <NavLink to="/about">By Matt Swain</NavLink>
+    <Box sx={{ display: ['block', 'flex']}}>
+      <Box sx={{ textAlign: ["center", "initial"] }}>
+        <NavLink to="/about">By Matt Swain</NavLink>
+      </Box>
       <div sx={{ mx: "auto" }} />
-      <NavLink href="https://twitter.com/mattswain123">Twitter</NavLink>
-      <NavLink href="https://github.com/mcs07">GitHub</NavLink>
-      <NavLink href="https://uk.linkedin.com/in/mcswain">LinkedIn</NavLink>
-    </Flex>
+      <Box sx={{ textAlign: ["center", "initial"] }}>
+        <NavLink href="https://twitter.com/mattswain123">Twitter</NavLink>
+        <NavLink href="https://github.com/mcs07">GitHub</NavLink>
+        <NavLink href="https://uk.linkedin.com/in/mcswain">LinkedIn</NavLink>
+      </Box>
+    </Box>
   </Container>
 )
 
